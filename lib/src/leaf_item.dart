787491fe32862080d037f681694ab41dbcf8b621
ps++ <43517405+msfm2018@users.noreply.github.com>
2022-05-30 11:39:05 +0800

    Add files via upload

--- lib/src/leaf_item.dart
+++ lib/src/leaf_item.dart
@@ -22,53 +22,72 @@ class LeafItem extends StatefulWidget {
 class _LeafItemState extends State<LeafItem> {
   @override
   Widget build(BuildContext context) {
-    return MouseRegion(
-        onEnter: (event) {
-          widget.bgColor = Colors.amberAccent;
-        },
-        child: SizedBox(
-          width: 200,
-          child: Column(
-            children: <Widget>[
-              Container(
-                // margin: EdgeInsets.only(left: 10),
-                decoration: BoxDecoration(color: widget.bgColor, borderRadius: const BorderRadius.only(bottomRight: Radius.circular(50), topRight: Radius.circular(50))),
-                // padding: const EdgeInsets.all(8),
-                child:
+    return
+        //  MouseRegion(
+        //     onEnter: (event) {
+        //       widget.bgColor = Colors.amberAccent;
+        //     },
+        //     child:
+        SizedBox(
+      width: 200,
+      child: Column(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            decoration: BoxDecoration(color: widget.bgColor, borderRadius: const BorderRadius.only(bottomRight: Radius.circular(50), topRight: Radius.circular(50))),
+            // padding: const EdgeInsets.all(8),
+            child:
 
-                    /// Material 关键点 响应 hoverColor
-                    Material(
-                        child: ListTile(
-                  dense: true,
-                  shape: RoundedRectangleBorder(
-                    // borderRadius: BorderRadius.circular(50),
-                    borderRadius: const BorderRadius.only(bottomRight: Radius.circular(50), topRight: Radius.circular(50)),
-                    // side: BorderSide(color: Colors.black),
-                  ),
-                  title: Text(
-                    widget.title,
-                    style: TextStyle(fontSize: widget.textSize),
-                  ),
-                  leading: widget.image,
-                  // trailing: Icon(Icons.keyboard_arrow_right),
-                  // leading: CircleAvatar(backgroundImage: NetworkImage('https://lupic.cdn.bcebos.com/20210629/3000001317_14.jpg')),
-                  hoverColor: Colors.red,
-                  enabled: true,
-                  onTap: () {
-                    widget.itemOnTap1(widget.node);
-                  },
-                )),
+                /// Material 关键点 响应 hoverColor
+                Material(
+                    child:
 
-                ///
+                        ///
+                        ListTile(
+              dense: true,
+              shape: RoundedRectangleBorder(
+                // borderRadius: BorderRadius.circular(50),
+                borderRadius: const BorderRadius.only(bottomRight: Radius.circular(50), topRight: Radius.circular(50)),
+                // side: BorderSide(color: Colors.black),
               ),
-              // ),
-              Container(
-                color: Colors.transparent,
-                height: 10.5,
+              title: Text(
+                widget.title,
+                style: TextStyle(
+                  fontSize: widget.textSize,
+                  fontWeight: FontWeight.w100,
+                ),
               ),
-            ],
+              leading: widget.image,
+              // trailing: Icon(Icons.keyboard_arrow_right),
+              // leading: CircleAvatar(backgroundImage: NetworkImage('https://lupic.cdn.bcebos.com/20210629/3000001317_14.jpg')),
+              hoverColor: Colors.red,
+              enabled: true,
+              onTap: () {
+                widget.itemOnTap1(widget.node);
+              },
+            )
+
+                    ///
+                    ),
+
+            ///
           ),
-        ));
+          // ),
+
+          // Container(
+          //   decoration: BoxDecoration(
+          //     border: Border(
+          //       bottom: BorderSide(
+          //         color: Colors.grey[800]!,
+          //         width: 2.0,
+          //       ),
+          //     ),
+          //   ),
+          // ),
+        ],
+      ),
+      // )
+    );
   }
 }
 
